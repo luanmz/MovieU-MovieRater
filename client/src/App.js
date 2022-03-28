1c5fb84e1773b7ad77458cea1a6a09ac9910c830
luanmz <luanpereiramz@gmail.com>
2022-03-27 22:44:40 -0300

    feat: adiconei a pasta features onde vão ficar as paginas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Login from './features/login/Login'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Login/>
     </div>
   );
 }
