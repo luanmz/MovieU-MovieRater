48a3f68a09b3034c82a35d9afd7e1625a26c7aeb
luanmz <luanpereiramz@gmail.com>
2022-03-29 02:04:30 -0300

    feat: criei pagina de registro e estilizei o login

--- client/src/features/login/Login.js
+++ client/src/features/login/Login.js
@@ -12,56 +12,75 @@ function Login() {
     const validationsLogin = yup.object().shape({
         email: yup
           .string()
-          .email("Email inválido")
-          .required("O email é obrigatório"),
+          .email("*Email inválido")
+          .required("*O email é obrigatório"),
         password: yup
           .string()
-          .min(6, "A senha deve ter pelo menos 6 caracteres")
-          .required("A senha é obrigatória"),
+          .min(6, "*A senha deve ter pelo menos 6 caracteres")
+          .required("*A senha é obrigatória"),
       });
 
     return (
       <div className="Login-container">
-        <h1>Log in</h1>
+        
+        <div className='imageContainer'></div>
+
         <Formik 
         initialValues={{}} 
         onSubmit={handleClickLogin} 
         validationSchema={validationsLogin}>
 
+            
+
             <Form className="login-form">
 
-                <div className="login-form-group">
-                    <Field name="email" className="form-field" placeholder="Email"></Field>
+              <div className='title'>
+               <h1>Login</h1>
+                <h2>Welcome again</h2>
+              </div> 
+              
 
-                    <ErrorMessage 
-                    component="span"
-                    name="email"
-                    className='form-error'
-                    />
-                    
-                </div>
+              <div className="login-form-group">
+                  <Field name="email" 
+                  className="form-field" 
+                  placeholder="Email"
+                  autocomplete="off"></Field>
 
-                <div className="login-form-group">
-                    <Field name="password" className="form-field" placeholder="Senha"></Field>
+                  <ErrorMessage 
+                  component="span"
+                  name="email"
+                  className='form-error'
+                  />
+                  
+              </div>
 
-                    <ErrorMessage 
-                    component="span"
-                    name="password"
-                    className='form-error'
-                    />
-                    
-                </div>
+              <div className="login-form-group">
+                  <Field name="password"
+                  type='password'
+                  className="form-field" 
+                  placeholder="Senha"></Field>
 
-                <button className='button-login' type='submit'>Login</button>
+                  <ErrorMessage 
+                  component="span"
+                  name="password"
+                  className='form-error'
+                  />
+                  
+              </div>
+
+              <button className='button-login' type='submit'>Login</button>
+              
+              <p className='register-link-text'>
+              Ainda não possui uma conta? 
+              <a href='register' target='_blank' > Sign up</a>
+              </p>
 
             </Form>
+            
 
         </Formik>
 
-        <p>
-            Ainda não possui uma conta? 
-            <a> Sign up</a>
-        </p>
+        
       </div>
     );
   }
