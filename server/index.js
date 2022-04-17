818b14076d9bf9536885ffc987972365c56f12b5
luanmz <luanpereiramz@gmail.com>
2022-04-17 00:02:23 -0300

    test: Testes no backend usando o axios

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const api = require("./api")
 const app = express();
+
+
+app.use(express.json())
 
 app.listen(3001, () => {
     console.log("Rodando na porta 3001")
 });
+
+app.get("/", (req, res) => {
+    return res.send("Rodando")
+})
+
+app.get("/pokemon/:id", async (req, res) => {
+    const {id} = req.params
+    const {data} = await api.get(`pokemon/${id}`)
+    
+    return res.send({nome: data.name})
+})
