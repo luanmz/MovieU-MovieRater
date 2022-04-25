8b2d3cccd127da7182ed2b2bfd3bc81adfcc5733
luanmz <luanpereiramz@gmail.com>
2022-04-24 22:51:54 -0300

    fix: Separação do front e do back

--- server/index.js
+++ server/index.js
@@ -4,12 +4,7 @@ const app = express();
 
 const API_KEY = "7745c2cd007513d18df83f01e36a3027"
 
-
 app.use(express.json())
-
-app.listen(3001, () => {
-    console.log("Rodando na porta 3001")
-});
 
 app.get("/", (req, res) => {
     return res.send("Rodando")
@@ -17,6 +12,10 @@ app.get("/", (req, res) => {
 
 app.get("/trending", async (req, res) => {
     const {data} = await api.get(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
-    
-    return res.send(data)
+    console.log(data)
+    return res.send()
 })
+
+app.listen(3001, () => {
+    console.log("Rodando na porta 3001")
+});
